6a14b8177dc723ac1d5225106f0c5e0d09891495
selvan <p.thamarai@gmail.com>
2021-06-23 13:15:26 +0530

    build script

---
+++ selvan_android.sh
@@ -0,0 +1,5 @@
+./scripts/make_android_all.sh
+cp /Users/selvan/code/amulet/builds/android_arm32/lua51/release/bin/libamulet.so /Users/selvan/code/AmuletPlayGround/app/src/main/jniLibs/armeabi-v7a
+cp /Users/selvan/code/amulet/builds/android_arm64/lua51/release/bin/libamulet.so /Users/selvan/code/AmuletPlayGround/app/src/main/jniLibs/arm64-v8a
+cp /Users/selvan/code/amulet/builds/android_x86/lua51/release/bin/libamulet.so /Users/selvan/code/AmuletPlayGround/app/src/main/jniLibs/x86
+cp /Users/selvan/code/amulet/builds/android_x86_64/lua51/release/bin/libamulet.so /Users/selvan/code/AmuletPlayGround/app/src/main/jniLibs/x86_64
